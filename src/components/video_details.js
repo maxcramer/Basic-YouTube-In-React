05597db2f8bae067695823e42b3e66226e932928
Max Cramer <maximiliancramer@yahoo.com>
2019-01-22 16:02:06 +0000

    videos on left clickable to view

--- src/components/video_details.js
+++ src/components/video_details.js
@@ -13,7 +13,7 @@ const VideoDetail = (props) => {
   return (
     <div className="video-detail col-md-8">
       <div className="embed-responsive embed-responsive-16by9">
-        <iframe className="embed-responsive-item" src={url} alt={video.snippet.title}></iframe>
+        <iframe className="embed-responsive-item" src={url}></iframe>
       </div>
       <div className="details">
         <div>{video.snippet.title}</div>
