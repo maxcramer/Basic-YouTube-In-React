25dfc4bb331e0ccbf17463b9f231690d6694e518
Max Cramer <maximiliancramer@yahoo.com>
2019-01-22 11:27:31 +0000

    inital commit, youtube video appearing in browser singular

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import YouTube from 'react-youtube';
 import './App.css';
 
-class App extends Component {
+class App extends React.Component {
   render() {
+    const opts = {
+      height: '390',
+      width: '640',
+      playerVars: { // https://developers.google.com/youtube/player_parameters
+        autoplay: 1
+      }
+    };
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <YouTube
+        videoId="2g811Eo7K8U"
+        opts={opts}
+        onReady={this._onReady}
+      />
     );
+  }
+  _onReady(event) {
+    // access to player in all event handlers via event.target
+    event.target.pauseVideo();
   }
 }
 
